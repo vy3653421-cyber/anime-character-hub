d1ffa08b64ac60c81f2f8ef5dde169dc7521d454
vy3653421-cyber <vy3653421@gmail.com>
2026-09-10 11:55:40 +0530

    refactor: route runtime IPC through handler module

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,17 +1,15 @@
-import { app, ipcMain } from "electron";
+import { app } from "electron";
+import { registerIpcHandlers } from "./ipc";
 import { createMainWindow } from "./window";
 
 app.whenReady().then(() => {
-  ipcMain.handle("mate:get-status", () => ({
-    ready: true,
-    version: app.getVersion(),
-  }));
+  registerIpcHandlers(app.getVersion());
 
   const window = createMainWindow();
   window.loadFile("src/renderer/index.html");
 
   app.on("activate", () => {
-    if (window.isDestroyed()) createMainWindow();
+    if (window.isDestroyed()) createMainWindow().loadFile("src/renderer/index.html");
   });
 });
 
