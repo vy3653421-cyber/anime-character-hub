e3053092e82dc51c549d276a7ff4d31cf1e76334
vy3653421-cyber <vy3653421@gmail.com>
2026-09-10 11:55:27 +0530

    refactor: export permission policy

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,4 +3,5 @@ export * from "./ai-provider";
 export * from "./avatar-state";
 export * from "./contracts";
 export * from "./memory";
+export * from "./permission-policy";
 export * from "./tool-registry";
